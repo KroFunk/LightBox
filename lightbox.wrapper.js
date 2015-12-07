65ddf33f74a89b93b89da36462f61c19462e9aa5
Robin Wright <robin@chalkcreation.co.uk>
2015-12-07 07:29:25 +0000

    Borders and X button position fixed

--- lightbox.wrapper.js
+++ lightbox.wrapper.js
@@ -9,7 +9,7 @@ var mydir = path.split('/').slice(0, -1).join('/')+'/';
 var lightBox = document.createElement("div");
 lightBox.id = 'lightBoxWrapper';
 document.body.appendChild(lightBox);
-document.getElementById('lightBoxWrapper').innerHTML = '<div id="grey" style="position:fixed; left:-5%; top:-5%; opacity:0.4; background:#000; width:110%; height:110%; display: none;" onclick="closewrapper();">&nbsp;</div><div id="lightBoxwrapper" style="background-color:rgba(255,255,255,0); border-radius: 0px; padding:0px; position: fixed; left: 50%; top: 50%; z-index: 999999; width:0px; height:0px; margin-left:0px; margin-top:0px; display: none;"><a href="javascript:void(0);" onclick="closewrapper();"><img id="lightBoxX" src="'+mydir+'X.png" style="position:relative; top:-0px; left:0px; border:0 none;"></a><div id="lightBoxBorder" style="background:#fff; border-radius:2px; margin-top:-10px; box-shadow:0px 0px 10px;"><iframe id="lightBox" style="border:1px solid #ddd; border-radius:2px; height:220px; width:100%;" border="0" frameborder="0"></lightBox></div></div>';
+document.getElementById('lightBoxWrapper').innerHTML = '<div id="grey" style="-webkit-transition: 0.5s; -moz-transition: 0.5s; transition: 0.5s;position:fixed; left:-5%; top:-5%; opacity:0.4; background:#000; width:110%; height:110%; display: none; visibility: hidden;" onclick="closewrapper();">&nbsp;</div><div id="lightBoxwrapper" style="-webkit-transition: 0.5s; -moz-transition: 0.5s; transition: 0.5s;background-color:rgba(255,255,255,0); border-radius: 0px; padding:0px; position: fixed; left: 50%; top: 50%; z-index: 999999; width:0px; height:0px; margin-left:0px; margin-top:0px; display: none; visibility: hidden;"><a href="javascript:void(0);" onclick="closewrapper();"><img id="lightBoxX" src="'+mydir+'X.png" style="-webkit-transition: 0.5s; -moz-transition: 0.5s; transition: 0.5s;position:relative; top:-0px; left:0px; border:0 none;"></a><div id="lightBoxBorder" style="-webkit-transition: 0.5s; -moz-transition: 0.5s; transition: 0.5s;background:#fff; border-radius:2px; margin-top:-10px; box-shadow:0px 0px 10px;"><iframe id="lightBox" style="-webkit-transition: 0.5s; -moz-transition: 0.5s; transition: 0.5s;border:1px solid #ddd; border-radius:2px; height:220px; width:100%;" border="0" frameborder="0"></lightBox></div></div>';
 
 // Stop scroll event 'bubble' - requires jQuery
 $('#lightBox').on('mousewheel DOMMouseScroll', function(ev) {
@@ -24,30 +24,35 @@ document.getElementById("lightBoxBorder").style.border = border+"px solid #FFF";
 }
 else {
 document.getElementById("lightBoxBorder").style.border = "none";
+border = 0;
 }
-
 
 // Show popup elements
 document.getElementById('grey').style.display='block';
 document.getElementById('lightBoxwrapper').style.display='block'; 
+document.getElementById('grey').style.visibility='visible';
+document.getElementById('lightBoxwrapper').style.visibility='visible'; 
 
 // Resize elements
-document.getElementById('lightBoxwrapper').style.width=x + "px";
-document.getElementById('lightBoxwrapper').style.height=y + "px";
-document.getElementById('lightBox').style.height=(y - 10) + "px";
+document.getElementById('lightBoxwrapper').style.width=x + ((2 * border) + 2) + "px";
+document.getElementById('lightBoxwrapper').style.height=y + (2 * border) + "px";
+document.getElementById('lightBox').style.height=(y) + "px";
+document.getElementById('lightBox').style.width=(x) + "px";
 
 // Position elements
-document.getElementById('lightBoxwrapper').style.marginLeft="-" + (x / 2) + "px";
-document.getElementById('lightBoxwrapper').style.marginTop="-" + (y / 2) + "px";
-document.getElementById('lightBoxX').style.left=(x - 10) + "px";
+document.getElementById('lightBoxwrapper').style.marginLeft="-" + ((x + (2 * border)) / 2) + "px";
+document.getElementById('lightBoxwrapper').style.marginTop="-" + ((y + (2 * border)) / 2) + "px";
+document.getElementById('lightBoxX').style.left=(x - (10 - (border * 2))) + "px";
 
 // Set Path on lightBox
 document.getElementById('lightBox').src = url;
 }
 
-function closewrapper() { //will close the window without refreshing the page. //will reload table data
+function closewrapper() { 
 // hide popup elements
  document.getElementById('lightBox').src = 'about:blank';
 document.getElementById('lightBoxwrapper').style.display='none'; 
 document.getElementById('grey').style.display='none';
+document.getElementById('grey').style.visibility='hidden';
+document.getElementById('lightBoxwrapper').style.visibility='hidden'; 
 }
